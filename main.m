e1576a2ce8edc7854514c3b91807376c60ecc4d5
Zifan Yu <FredYu@wireless-10-104-39-150.umd.edu>
2019-11-08 17:52:12 -0500

    preypred

--- main.m
+++ main.m
@@ -30,7 +30,7 @@ c1 = (1/30);        % death rate of butterfly
 
 % Time variable
 Tmax = 1000;
-deltaT = 0.01;
+deltaT = 0.0001;
 time = (0:deltaT:Tmax);
 Nt = length(time);
 
@@ -39,20 +39,20 @@ B = zeros(Nt,1);
 W = zeros(Nt,1);
 
 % Initial conditions
-B(1) = 200;
+B(1) = 500;
 W(1) = 100;
-
+V = a2*B(1)-d2*log(B(1))+d1*W(1)-a1*log(W(1))
 % Implement Euler's scheme
 for it = 2:Nt
     t = time(it);
     % First order update
     B(it) = B(it-1) + (a1*B(it-1)- (d1*B(it-1)*W(it-1)) - c1*B(it-1))*deltaT;
-    % B(it) = B(it-1) + (a1*B(it-1)- (d1*B(it-1)*W(it-1)))*deltaT;
+    %B(it) = B(it-1) + (a1*B(it-1)- (d1*B(it-1)*W(it-1)))*deltaT;
 
     W(it) = W(it-1) + (a2*B(it-1)*W(it-1) - d2 *W(it-1))*deltaT;
 end
 
-
+V2=a2*B(it)-d2*log(B(it))+d1*W(it)-a1*log(W(it))
 
 % Visualize solution
 plot(time,B,'b',time,W,'r')
@@ -61,8 +61,8 @@ legend('Butterfly','Wasp')
 
 % Collect statistics
 % Max Infected time
-[InfectMax, imax] = max(I);
-tMaxInfected = time(imax);
+%[InfectMax, imax] = max(I);
+%tMaxInfected = time(imax);
 
 % Report results
-fprintf(1,'Max Outbreak at t=%f , Infected = %f\n',tMaxInfected,InfectMax);
+%fprintf(1,'Max Outbreak at t=%f , Infected = %f\n',tMaxInfected,InfectMax);
